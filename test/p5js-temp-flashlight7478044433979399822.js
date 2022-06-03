324f13da550eb1368f8a67f25dfd0e5c68bc29f3
hanriverkitty <danieltls@naver.com>
2022-06-03 13:14:50 +0900

    makefile

---
+++ test/p5js-temp-flashlight7478044433979399822.js
@@ -0,0 +1,51 @@
+let img;
+
+function preload() {
+  img = loadImage('./data/6.png');
+}
+
+function setup() {
+
+  let cnv = createCanvas(1280, 720);
+  cnv.parent('smallC');
+  image(img, 0, 0, 1280, 720);
+  pixelDensity(1);
+}
+
+function draw() {
+
+  loadPixels();
+  let rad = 90;
+  img.loadPixels();
+  for (let y = 0; y < height; y++) {
+    for (let x = 0; x < width; x++) {
+      let pos = (x + y * width) * 4;
+      let r = img.pixels[pos];
+      let g = img.pixels[pos + 1];
+      let b = img.pixels[pos + 2];
+
+      let d = dist(x, y, mouseX, mouseY);
+
+
+      let adjustBrightness = map(d, 0, rad, 1, 0);
+      r *= adjustBrightness;
+      g *= adjustBrightness;
+      b *= adjustBrightness;
+
+
+      r = constrain(r, 0, 255);
+      g = constrain(g, 0, 255);
+      b = constrain(b, 0, 255);
+
+
+      pixels[pos] = r;
+      pixels[pos + 1] = g;
+      pixels[pos + 2] = b;
+      pixels[pos + 3] = 255;
+    }
+  }
+
+  updatePixels();
+}
+
+
